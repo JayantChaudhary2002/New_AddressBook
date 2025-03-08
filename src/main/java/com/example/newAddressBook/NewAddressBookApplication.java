7b9f41fa9208fd778c366ca35e5b330d6cd05e82
Jayant Chaudhary <jayantchaudhary2002@gmail.com>
2025-03-08 11:38:25 +0530

    Project Setup

---
+++ src/main/java/com/example/newAddressBook/NewAddressBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.newAddressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAddressBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newAddressBook/NewAddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newAddressBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
