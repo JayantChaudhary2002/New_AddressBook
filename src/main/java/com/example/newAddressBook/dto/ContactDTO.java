d52b672897e1f91c627218e995d86cb8bb267d15
Jayant Chaudhary <jayantchaudhary2002@gmail.com>
2025-03-08 14:51:16 +0530

    Generating auto getter and setter

--- src/main/java/com/example/newAddressBook/dto/ContactDTO.java
+++ src/main/java/com/example/newAddressBook/dto/ContactDTO.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+@Data //Allows to generate auto setters and getters
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContactDTO {
